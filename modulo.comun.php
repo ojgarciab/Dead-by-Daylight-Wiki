df510975ed7c266eabf7f1176e88f53710c33642
Oscar Garcia <oscar.github@linaresdigital.com>
2020-12-31 17:02:27 +0100

    Convertido en función

--- modulo.comun.php
+++ modulo.comun.php
@@ -1,16 +1,19 @@
 <?php
-/* Cargamos los archivos de datos de cada lenguaje */
-$datos = [];
-foreach($lenguajes as $lenguaje) {
-  $datos['traduccion'][$lenguaje] = json_decode(cache_url(
-    "https://raw.githubusercontent.com/Masusder/DataTrackerDBD/master/Content/Localization/DeadByDaylight/{$lenguaje}/DeadByDaylight.json",
-    '.DeadByDaylight.json.'. $lenguaje,
-    $refrescar
-  ))->{''};
-  $datos['normalizado'][$lenguaje] = array_map('mb_strtolower', (array)$datos['traduccion'][$lenguaje]);
-  $datos['contenido'][$lenguaje] = json_decode(cache_url(
-    obtener_url($lenguaje, 'Module:Datatable/Perks'),
-    'Module-Datatable-Perks.'. $lenguaje,
-    $refrescar
-  ))->query->pages[0]->revisions[0]->slots->main->content;
+function cargar_datos($modulo, $lenguajes, $refrescar = false) {
+    /* Cargamos los archivos de datos de cada lenguaje */
+    $datos = [];
+    foreach($lenguajes as $lenguaje) {
+    $datos['traduccion'][$lenguaje] = json_decode(cache_url(
+        "https://raw.githubusercontent.com/Masusder/DataTrackerDBD/master/Content/Localization/DeadByDaylight/{$lenguaje}/DeadByDaylight.json",
+        '.DeadByDaylight.json.'. $lenguaje,
+        $refrescar
+    ))->{''};
+    $datos['normalizado'][$lenguaje] = array_map('mb_strtolower', (array)$datos['traduccion'][$lenguaje]);
+    $datos['contenido'][$lenguaje] = json_decode(cache_url(
+        obtener_url($lenguaje, $modulo),
+        'Module-Datatable-Perks.'. $lenguaje,
+        $refrescar
+    ))->query->pages[0]->revisions[0]->slots->main->content;
+    }
+    return $datos;
 }
